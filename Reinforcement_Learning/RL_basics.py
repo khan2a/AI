a394aeee9a369107b6c1a3dbb102677a089bbce1
khan2a <engineer.atique@gmail.com>
2020-06-27 21:56:35 +0400

    Working for most of the games

--- Reinforcement_Learning/RL_basics.py
+++ Reinforcement_Learning/RL_basics.py
@@ -1,1 +1,12 @@
-# this is to test some basic codes
+import gym
+from gym import envs
+envids = [spec.id for spec in envs.registry.all()]
+for envid in sorted(envids):
+    print(envid)
+
+env = gym.make('CartPole-v0')
+env.reset()
+for _ in range(100):
+    env.render(mode='rgb_array')
+    env.step(env.action_space.sample())
+env.env.close()
